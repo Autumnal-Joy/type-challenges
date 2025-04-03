6bd5c1edf8b577ad372fe5bbaa8e73dab5337c40
AutJ <autumnal_joy@foxmail.com>
2025-04-04 00:19:17 +0800

    00898-easy-includes

--- playground/easy/00898-easy-includes.ts
+++ playground/easy/00898-easy-includes.ts
@@ -18,7 +18,8 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Includes<T extends readonly any[], U> = any
+type Includes<T extends readonly any[], U> = T extends readonly [infer F, ...infer Rest] ?
+  Equal<F, U> extends true ? true : Includes<Rest, U> : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'

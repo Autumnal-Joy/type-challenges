9013512cd7e90cbb276faddad56cbef9de49695b
AutJ <autumnal_joy@foxmail.com>
2025-04-04 16:51:28 +0800

    00020-medium-promise-all

--- playground/medium/00020-medium-promise-all.ts
+++ playground/medium/00020-medium-promise-all.ts
@@ -23,7 +23,11 @@
 
 /* _____________ Your Code Here _____________ */
 
-declare function PromiseAll(values: any): any
+declare function PromiseAll<T extends any[]>(
+  values: [...T]
+): Promise<{
+  [K in keyof T]: Awaited<T[K]>
+}>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
